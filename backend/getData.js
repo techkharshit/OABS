df5d5bc98077673e3f19aa89f3b473627b86e491
kharshit <155631133+techkharshit@users.noreply.github.com>
2024-11-18 09:37:50 +0530

    Delete backend directory

--- backend/getData.js
+++
@@ -1,8 +0,0 @@
-module.exports = async function (req, res, collection) {
-  var today = new Date().toLocaleDateString("en-US");
-  let result = await collection
-    .find({ hall: req.query.hall, date: { $gte: today } })
-    .toArray();
-  console.log(result);
-  return res.status(200).json(result);
-};
